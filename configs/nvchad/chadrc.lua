460cfc4d893f721c78477c8d64382b7c44e5889c
xdays <easedays@gmail.com>
2022-07-26 13:13:19 +0000

    update nvchad config

--- configs/nvchad/chadrc.lua
+++ configs/nvchad/chadrc.lua
@@ -1,17 +1,18 @@
 local M = {}
 
-local plugin_conf = require("custom.plugins.configs")
-local user_plugins = require("custom.plugins")
+local pluginConfs = require("custom.plugins.configs")
 
 M.plugins = {
-    status = { alpha = true, colorizer = true },
-    install = user_plugins,
-    default_plugin_config_replace = { nvim_treesitter = plugin_conf.treesitter },
-    default_plugin_remove = { "akinsho/bufferline.nvim" },
+    options = {
+      lspconfig = {
+        setup_lspconf = "custom.plugins.lspconfig",
+      },
+    },
+    override = { ["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter },
+    user = require("custom.plugins"),
 }
 
 M.ui = {
-    -- theme = "onedark",
     theme = "monekai",
 }
 
--- configs/nvchad/plugins/init.lua
+++ configs/nvchad/plugins/init.lua
@@ -1,16 +1,17 @@
 return {
-    {
-        "jose-elias-alvarez/null-ls.nvim",
+    ["jose-elias-alvarez/null-ls.nvim"] = {
         after = "nvim-lspconfig",
         config = function()
             require("custom.plugins.null-ls").setup()
         end,
     },
-    {
-        "williamboman/nvim-lsp-installer",
+    ["williamboman/nvim-lsp-installer"] = {
+        after = "nvim-lspconfig",
         config = function()
             require("custom.plugins.lsp-installer").setup()
         end,
     },
-    { "rcarriga/nvim-notify" },
+    ["goolord/alpha-nvim"] = {
+         disable = false,
+    },
 }
