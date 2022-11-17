266ac85b72d2846db0242f924ce2c1002094fe6b
xdays <easedays@gmail.com>
2022-11-17 11:59:45 +0800

    fix soft link

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,24 @@ export WROOT=$PWD
 
 # install deps
 install_tools() {
-    if [ "$(uname)" == "Darwin" ]; then
+    if [[ "$(uname)" == "Darwin" ]]; then
         if ! command -v brew &> /dev/null; then
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         fi
         brew tap homebrew/cask
         brew tap homebrew/bundle
         cd $WROOT/deps && brew bundle && cd -
-    elif [ -e /etc/redhat-release ]; then
+    elif [[ -e /etc/redhat-release ]]; then
         sudo yum install -y epel-release
         sudo yum install -y $(cat ${WROOT}/deps/tools.redhat)
-    else
+    elif [[ -e /etc/debian_version ]]; then
         sudo apt -qq update
         sudo DEBIAN_FRONTEND=noninteractive apt -qq install -y $(cat ${WROOT}/deps/tools.debian)
     fi
 }
 
 config_tools() {
-    for f in $(find $WROOT/configs -type f); do
+    for f in $(find $WROOT/configs -maxdepth 1 -type f); do
         filename=$(basename "$f")
         src="$WROOT/configs/$filename"
         dest=$HOME/.$filename
@@ -32,8 +32,7 @@ config_tools() {
 }
 
 config_vim() {
-    if [ -e $HOME/.vim ]
-    then
+    if [[ -e $HOME/.vim ]]; then
         echo "vim is already configured"
     else
         mkdir -p $HOME/.vim
@@ -45,8 +44,7 @@ config_vim() {
 }
 
 config_neovim() {
-    if [ -e $HOME/.config/nvim/lua/custom ]
-    then
+    if [[ -e $HOME/.config/nvim/lua/custom ]]; then
         echo "vim is already configured"
     else
         git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
@@ -55,7 +53,7 @@ config_neovim() {
 }
 
 config_zsh() {
-    if [ -e $HOME/.oh-my-zsh ]; then
+    if [[ -e $HOME/.oh-my-zsh ]]; then
         echo "zsh is already configured"
     else
         git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
--- plugins/brew.sh
+++ plugins/brew.sh
@@ -1,4 +1,4 @@
-if [ $(arch) = "x86_64" ] && [ $USER = "xdays" ]; then
+if [[ $(uname -m) = "x86_64" ]] && [[ $USER = "xdays" ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     export BREW_BIN=$HOMEBREW_PREFIX/bin
 fi
