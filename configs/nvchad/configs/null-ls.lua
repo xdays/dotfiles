c7f8c98a54c5329442bb31e0eaa42d9a20ff0486
xdays <easedays@gmail.com>
2023-09-05 10:24:53 +0800

    update nvchad config

---
+++ configs/nvchad/configs/null-ls.lua
@@ -0,0 +1,31 @@
+local null_ls = require("null-ls")
+
+local b = null_ls.builtins
+
+local sources = {
+
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+
+	-- Lua
+	b.formatting.stylua,
+
+	-- cpp
+	b.formatting.clang_format,
+
+	-- terraform
+	b.formatting.terraform_fmt,
+}
+
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
+	end,
+})
---
+++ configs/nvchad/mappings.lua
@@ -0,0 +1,13 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+}
+
+-- more keybinds!
+
+return M
+
