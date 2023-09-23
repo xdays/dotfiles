cc9b00f119ef2d491ec149d7f00643e0c0a36e3e
xdays <easedays@gmail.com>
2023-09-23 11:56:36 +0800

    update

--- plugins/kubectl.sh
+++ plugins/kubectl.sh
@@ -1,7 +1,15 @@
 # config kubernetes
 export KUBECONFIG=$(for i in ~/.kube/c-*;do echo -n "$i":;done)~/.kube/config
-#export KUBE_EDITOR="vim"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+kubectl() {
+    if [ -e ~/workspace/bin/kubectl ]; then
+        CMD=~/workspace/bin/kubectl
+    else
+        CMD=$BREW_BIN/kubectl
+    fi
+    $CMD --context="$KUBE_PROFILE" --namespace "$KUBE_NAMESPACE" "$@"
+}
 
 helm() {
     if [ -e ~/workspace/bin/helm ]; then
@@ -12,11 +20,19 @@ helm() {
     $CMD --kube-context="$KUBE_PROFILE" "$@"
 }
 
-kube-job-log() {
+egp() {
+  echo $KUBE_PROFILE
+}
+
+kns() {
+  export KUBE_NAMESPACE=${1:-default}
+}
+
+kjl() {
     kubectl logs $(kubectl get pods -a --selector=job-name="$1" --output='jsonpath={.items..metadata.name}')
 }
 
-kube-rename-context() {
+krc() {
     for context in $(kubectl config get-contexts | grep arn | sed 's/*//' | awk '{print $1}'); do
         if echo "$context" | grep -q '/'; then
             new=$(echo "$context" | awk -F '/' '{print $2}')
@@ -27,7 +43,7 @@ kube-rename-context() {
     done
 }
 
-kube-exec() {
+kexe() {
     cmd=${2:-"bash"}
     kubectl exec -it "$1" -- "$cmd"
 }
